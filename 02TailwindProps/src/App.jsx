d5ee4ad782224d8a6f94bc81048fe9e160a8884b
Vishal <vsaxena7033@gmail.com>
2024-09-01 15:03:47 +0530

    tailwind Done

--- 02TailwindProps/src/App.jsx
+++ 02TailwindProps/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Card from './component/Card'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1 className='bg-green-300'>Taiwind Test By Vishal</h1>
+      <Card username="vishal"></Card>
     </>
   )
 }
